6400bf33c8632e27cbc8ae34d0d82b71e3f54434
dolores91 <doloresalemang@gmail.com>
2023-09-02 14:17:55 -0300

    faq

---
+++ pages/api/faq.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { faqsData } from "../../components/faqs/faqsData";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === "GET") {
+        res.status(200).json(faqsData);
+    } else {
+        res.status(400).json({ error: "Método no permitido" });
+    }
+}
+
+
