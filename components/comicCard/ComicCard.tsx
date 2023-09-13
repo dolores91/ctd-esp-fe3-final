355fead56386f54e549ebc0487dbd4dd1f9aff1d
dolores91 <doloresalemang@gmail.com>
2023-09-13 13:14:50 -0300

    pagination

---
+++ components/comicCard/ComicCard.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react';
+import Link from 'next/link';
+import { Card, Typography, Button, CardMedia, CardContent, CardActions } from '@mui/material';
+
+interface IComicCardProps {
+    title: string;
+    image: string;
+    id: number;
+}
+
+const ComicCard: FC<IComicCardProps> = ({ title, image, id }) => {
+    return (
+        <Card variant="outlined">
+            <CardMedia
+                component="img"
+                height="194"
+                image={image}
+                alt={title}
+                sx={{ objectFit: 'contain' }}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="body2" component="div">
+                    {title}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small" variant="contained">
+                    COMPRAR
+                </Button>
+                <Link href={`/comics/${id}`}>
+                    <Button size="small" variant="outlined">
+                        VER DETALLE
+                    </Button>
+                </Link>
+            </CardActions>
+        </Card>
+    );
+};
+
+export default ComicCard;
---
+++ components/grid/Grid.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react'
+import { Grid } from '@mui/material';
+import { IComic } from 'types/IComic.type';
+import ComicCard from '../comicCard/ComicCard';
+
+interface GridProps {
+    comics: IComic[],
+}
+
+const GridLayout: FC<GridProps> = ({ comics }) => {
+
+    return (
+        <Grid container spacing={{ xs: 2 }} sx={{ marginBottom: "15px" }}>
+            {comics.map((comic, index) => (
+                <Grid item xs={12} md={6} lg={4} key={index}>
+                    <ComicCard title={comic.title} image={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} id={comic.id}></ComicCard>
+                </Grid>
+            ))}
+        </Grid>
+    )
+}
+
+export default GridLayout
